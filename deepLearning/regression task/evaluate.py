e833d30aafa9ac5dc92e76274fdb65da8f6d3138
Ossian <ossek829@student.liu.se>
2026-06-22 09:37:58 +0200

    add evaluation script to regression model, generate figure

---
+++ deepLearning/regression task/evaluate.py
@@ -0,0 +1,28 @@
+import csv
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--input_csv_file", required=True)
+    parser.add_argument("-o", "--output_file", default="evaluation_fig.png")
+    args = parser.parse_args()
+    
+    pred_radii = []
+    pred_pitches = []
+    corr_radii = []
+    corr_pitches = []
+    with open(args.input_csv_file, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            pred_radii.append(row["pred_radius"])
+            pred_pitches.append(row["pred_pitch"])
+            corr_pitches.append(row["correct_pitch"])
+            corr_radii.append(row["correct_radius"])
+
+    fig, ax = plt.subplots()
+    ax.plot(pred_radii, corr_radii, ".-")
+    ax.plot(pred_pitches, corr_pitches, ".-")
+    plt.savefig(args.output_file)
+    
+main()
